e3a26285b07e4de49330d4328062568f0d7e1169
cjduhamel <speed@duhamelandassociates.com>
2024-01-29 22:59:31 -0800

    added POST method

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -26,6 +26,11 @@ const findUserByName = (name) => {
 const findUserById = (id) =>
   users["users_list"].find((user) => user["id"] === id);
 
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+
 app.get("/users/:id", (req, res) => {
   const id = req.params["id"]; //or req.params.id
   let result = findUserById(id);
@@ -45,6 +50,12 @@ app.get("/users", (req, res) => {
     } else {
         res.send(users);
     }
+});
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
 });
 
 const users = {
