a23b6cc96bccb7d1e27aa3f5023f0d2d2b772ed9
cjduhamel <speed@duhamelandassociates.com>
2024-01-31 23:07:55 -0800

    they be talkin!

--- packages/react-frontend/src/MyApp.js
+++ packages/react-frontend/src/MyApp.js
@@ -1,5 +1,6 @@
 // src/MyApp.js
-import React, {useState} from "react";
+
+import React, {useState, useEffect} from 'react';
 import Table from "./Table";
 import Form from "./Form";
 
@@ -8,6 +9,14 @@ import Form from "./Form";
 
 function MyApp() {
     const [characters, setCharacters] = useState([]);
+
+    useEffect(() => {
+      fetchUsers()
+        .then((res) => res.json())
+        .then((json) => setCharacters(json["users_list"]))
+        .catch((error) => { console.log(error); });
+    }, [] );
+    
       function removeOneCharacter(index) {
         const updated = characters.filter((character, i) => {
           return i !== index;
@@ -30,7 +39,19 @@ function MyApp() {
           
         </div>
       );
+
+      
+
+      function fetchUsers() {
+          const promise = fetch("http://localhost:8000/users");
+          return promise;
+      }
+
+      
+      
 }
+
+
 
 
   
